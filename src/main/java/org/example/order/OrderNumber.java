7528a93bbfd3e4502ea5b1a774a383d3da44acce
Artem Rozhnov <bloodypilot@yandex.ru>
2024-01-22 00:47:42 +0300

    first

---
+++ src/main/java/org/example/order/OrderNumber.java
@@ -0,0 +1,13 @@
+package org.example.order;
+
+public class OrderNumber {
+    private int number;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
